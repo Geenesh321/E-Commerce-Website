dd8c6c19917bac80b0c18a379a390be0b86ed0ff
Geenesh Acharya <geenesh321@gmail.com>
2023-04-18 22:55:12 +0545

    Admin Panal and Changes in Login and Registration

---
+++ Admin.js
@@ -0,0 +1,29 @@
+const uploadInput = document.getElementById('upload');
+const imageContainer = document.querySelector('.image-upload-container');
+
+uploadInput.addEventListener('change', function () {
+    const file = this.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener('load', function () {
+        imageContainer.style.backgroundImage = `url(${reader.result})`;
+    });
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+});
+
+// Admin Table
+const deleteButtons = document.querySelectorAll('.delete-btn');
+deleteButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const row = event.target.closest('tr');
+    const confirmDelete = confirm('Are you sure you want to delete this product?');
+    if (confirmDelete) {
+      row.remove();
+    }
+  });
+});
+
+
